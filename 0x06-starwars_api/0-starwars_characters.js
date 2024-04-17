20a1784ae40ba24452e84f7d1466168749512eb7
lisapowell1991 <lisapowell46@gmail.com>
2024-04-17 05:42:14 -0700

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,40 @@
 #!/usr/bin/node
+
+// Load request module
+const request = require('request');
+
+// Read movieID from 1st command line arg
+const movieId = process.argv[2];
+
+// Function to fetch movie data from SWAPI
+function fetchMovieData(movieId) {
+	// Make an HTTP GET request to SWAPI film endpoint
+	const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+	request(url, { json:true }, (error, response, body) => {
+		if (error)
+			return console.log('Error:', error);
+
+		if (response.statusCode !== 200)
+			return console.log('Failed to fetch data:', response.statusCode);
+
+		// Printing each character name
+		body.characters.forEach(characterUrl => {
+			fetchCharacterName(characterUrl);
+		});
+	});
+}
+
+// Function to fetch character names
+function fetchCharacterName(url) {
+	request(url, { json: true }, (error, response, body) => {
+		if (error) {
+			console.log('Error:', error);
+			return;
+		}
+		console.log(body.name);
+	});
+}
+
+// Call the fetchMovieData function
+fetchMovieData(movieId);
