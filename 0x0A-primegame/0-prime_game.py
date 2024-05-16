81fff818f889c4b3cc7d700ad68909d91be40be1
lisapowell1991 <lisapowell46@gmail.com>
2024-05-16 07:44:41 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,70 +1,61 @@
+#!/usr/bin/python3
+
+""" A module containing a isWinner function """
+
+
 def isWinner(x, nums):
     """
     Determine the winner of each game round.
-
     Args:
     x (int): Number of rounds.
     nums (list): List of integers, where each integer n,
-    represents the upper limit of the set for each round.
-
+    represents the upper limit of the set of each round.
     Returns:
     str: The name of the player that won the most rounds,
     OR None if the result is a tie.
     """
 
-    def sieve_of_eratosthenes(max_num):
+    def sieve(n):
         """
-        Generate a list indicating prime status,
-        for numbers from 0 to max_num.
+        Generate a list of prime nums up to n,
+        using the Sieve of Eratosthenes.
+        Args:
+        n (int): The upper limit to generate prime nums.
+        Returns:
+        list: A list of prime nums up to n.
         """
-        is_prime = [True] * (max_num + 1)
+        primes = [True for _ in range(n + 1)]
         p = 2
-        while (p * p <= max_num):
-            if is_prime[p]:
-                for i in range(p * p, max_num + 1, p):
-                    is_prime[i] = False
+
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
             p += 1
-        is_prime[0], is_prime[1] = False, False # 0 and 1 are not prime nums
-        return is_prime
+        return [p for p in range(2, n + 1) if primes[p]]
 
-    # Find the maximum value of n to use in the Sieve of Eratosthenes
-    max_n = max(nums)
-
-    # Get the prime status of each number up to max_n
-    is_prime = sieve_of_eratosthenes(max_n)
-
-    # To keep track of the number of rounds won by Maria and Ben
-    maria_wins = 0
-    ben_wins = 0
-
-    # Function to count the number of prime moves possible
-    def count_prime_moves(n):
-        count = 0
-        for num in range(2, n + 1):
-            if is_prime[num]:
-                count += 1
-                # Mark multiples of this prime as non-prime
-                for multiple in range(num * 2, n + 1, num):
-                    is_prime[multiple] = False
-        return count
+    # Variables to count the num of wins for Maria and Ben
+    mariaWins = 0
+    benWins = 0
 
     # Simulate each round
     for n in nums:
-        # We need to recompute the prime array for each game
-        is_prime = sieve_of_eratosthenes(n)
+        primes = sieve(n)
+        turn = 0
 
-        prime_moves = count_prime_moves(n)
+        while primes:
+            prime = primes[0]
+            primes = [p for p in primes if p % prime != 0]
+            turn += 1
 
-        # If prime_moves is even,Ben win because Maria can't make the last move
-        # If prime_moves is odd, Maria wins because she makes the last move
-        if prime_moves % 2 == 1:
-            maria_wins += 1
+        if turn % 2 == 0:
+            benWins += 1
         else:
-            ben_wins += 1
+            mariaWins += 1
 
-    if maria_wins > ben_wins:
+    if mariaWins > benWins:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif benWins > mariaWins:
         return "Ben"
     else:
         return None
